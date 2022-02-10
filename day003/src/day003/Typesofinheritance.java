6ea1278406aa02b832aa6d2900850ee8b0a51a79
HP <HP@LAPTOP-MBU8NU9S>
2022-02-10 07:54:14 +0530

    Types of inheritance

---
+++ day003/src/day003/Typesofinheritance.java
@@ -0,0 +1,22 @@
+package day003;
+
+class Parent1{
+
+}
+class Parent2 extends Parent1{//child 1 of parent 1
+	
+}
+class Parent3 extends Parent2{// child of parent 2
+	
+}
+
+public class Typesofinheritance extends Parent1{//child 2 of parent 1
+	
+	
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
