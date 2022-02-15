52e060cad0b4ca902eca9b89b11035fff1b5c327
HP <HP@LAPTOP-MBU8NU9S>
2022-02-15 17:26:58 +0530

    15FEB Interface

---
+++ Interface/src/first/InterfaceDemo.java
@@ -0,0 +1,23 @@
+package first;
+
+
+interface A {
+	
+	void m1();
+}
+
+interface B {
+	
+	//void m2() {
+		// we not given body to the method in interface
+	//}
+}
+
+public class InterfaceDemo implements A,B {
+
+	public static void main(String[] args) {
+		//we can not create object of interface 
+
+	}
+
+}
